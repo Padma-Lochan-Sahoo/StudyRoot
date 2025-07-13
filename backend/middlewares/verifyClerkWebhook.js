3afd60ade863c06e1e451d50813b99badb235dd1
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-07-13 20:54:57 +0530

    check : middleware

--- backend/middlewares/verifyClerkWebhook.js
+++ backend/middlewares/verifyClerkWebhook.js
@@ -1,18 +1,24 @@
 // verifyClerkWebhook.js
 import { Webhook } from "svix";
 
-export const verifyClerkWebhook = async (req, res, next) => {
-  const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+// export const verifyClerkWebhook = async (req, res, next) => {
+//   const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-  let evt;
+//   let evt;
 
-  try {
-    evt = wh.verify(req.rawBody, req.headers);
-  } catch (err) {
-    console.error(err);
-    return res.status(400).send("Invalid webhook signature");
-  }
+//   try {
+//     evt = wh.verify(req.rawBody, req.headers);
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(400).send("Invalid webhook signature");
+//   }
 
-  req.body = evt;
+//   req.body = evt;
+//   next();
+// };
+
+export const verifyClerkWebhook = (req, res, next) => {
+  console.log("Skipping signature check for local test");
   next();
 };
+
