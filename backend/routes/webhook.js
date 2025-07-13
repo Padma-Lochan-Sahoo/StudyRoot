05e8e9017dc0ddc4fa28545e245a626388132ea3
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-07-13 09:45:27 +0530

    backend with clerk

--- backend/routes/webhook.js
+++ backend/routes/webhook.js
@@ -1,9 +1,29 @@
 import express from "express";
-import { handleClerkWebhook } from "../controllers/clerkWebhookController.js";
-import { verifyClerkWebhook } from "../middlewares/verifyClerkWebhook.js";
-
 const router = express.Router();
+import User from "../models/User.js"; 
 
-router.post("/clerk", verifyClerkWebhook, handleClerkWebhook);
+router.post("/clerk", async (req, res) => {
+  const eventType = req.body.type;
 
-export default router;
+  if (eventType === "user.created") {
+    const { id, email_addresses, first_name, last_name } = req.body.data;
+
+    const email = email_addresses[0]?.email_address;
+
+    try {
+      await User.create({
+        clerkId: id,
+        email,
+        firstName: first_name,
+        lastName: last_name,
+      });
+      res.status(200).send("User created");
+    } catch (err) {
+      res.status(500).send("Error creating user");
+    }
+  } else {
+    res.status(200).send("Unhandled event");
+  }
+});
+
+module.exports = router;
